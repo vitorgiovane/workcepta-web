6b5f50c1e9abb3014f0c65a9bb4e31ee69a9a83f
Vitor <vitorgpalves@gmail.com>
2019-07-08 01:40:07 -0300

    feature: lista o nome de usuários

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import axios from "axios"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const api = axios.create({
+  baseURL: "http://localhost:8010/api"
+})
+
+class App extends Component {
+  state = {
+    users: []
+  }
+
+  async componentDidMount() {
+    const { data: users } = await api.get("/users")
+    this.setState({ users })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.users.map(user => (
+            <li key={user.id}>{user.name}</li>
+          ))}
+        </ul>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
